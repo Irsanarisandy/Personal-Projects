3f2a0ee512dd5a12b62cc6f2e18374bd8d9d6483
Irsan Arisandy <irsanarisandy@hotmail.com>
2015-12-12 19:39:38 +1300

    About + Fancybox

---
+++ RYDesign/RYDesign/jQuery_Plugins/fancybox(setting).js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $(".fancybox").fancybox({
+        openEffect: 'elastic',
+        openSpeed: 150,
+
+        closeEffect: 'elastic',
+        closeSpeed: 150
+    });
+});
