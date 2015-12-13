434a59e9c025371b9891746242494b439c799c4f
Irsan Arisandy <irsanarisandy@hotmail.com>
2015-12-13 13:22:05 +1300

    Edit About & get more product pics

---
+++ RYDesign/RYDesign/jQuery_Plugins/galleria(setting).js
@@ -0,0 +1,9 @@
+﻿Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
+Galleria.run('.galleria', {
+    autoplay: 3000,
+    transition: 'fade',
+    imageCrop: 'width',
+    showImagenav: false,
+    thumbnails: false,
+    showCounter: false
+});
